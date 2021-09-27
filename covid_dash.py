5cdc70c582e5d1c30f60f0480e167068be93ca95
Marcos Huerta <sirmarcos@gmail.com>
2021-09-27 19:57:33 -0400

    limit calculations

--- covid_dash.py
+++ covid_dash.py
@@ -21,37 +21,49 @@ class dataLoader:
 
         self.dbc = create_engine('sqlite:///data_cache/covid_dash.db')
 
-    def get_data(self, state_list):
+    def get_data(self, state_list, cases=True):
         """get the data from the sql tables"""
         # maybe I should have one table with an id row for state vs county
         county_list = [x for x in state_list if ',' in x]
         state_list = [x for x in state_list if ',' not in x]
         binding_string = ','.join(['?'] * len(state_list))
-
-        data_states = pd.read_sql(
-            f"select * from states where state in ({binding_string})",
-            self.dbc,
-            params=state_list)
+        if cases:
+            data_states = pd.read_sql(
+                f"select date, case_growth, state, case_growth_per_100K, cases from states where state in ({binding_string})",
+                self.dbc,
+                params=state_list)
+        else:
+            data_states = pd.read_sql(
+                f"select date, new_deaths, state, new_deaths_per_100k, deaths from states where state in ({binding_string})",
+                self.dbc,
+                params=state_list)
         data_states['date'] = pd.to_datetime(data_states['date'])
         binding_string = ','.join(['?'] * len(county_list))
-
-        data_counties = pd.read_sql(
-            f'select c.*, p.population from counties c left join county_population p on c.fips = p.fips where c.state in ({binding_string});',
-            con=self.dbc,
-            params=county_list)
-        data_counties['date'] = pd.to_datetime(data_counties['date'])
-        data_counties.sort_values('date', inplace=True)
-        data_counties['case_growth'] = data_counties.groupby(
-            ['state'])['cases'].shift(0) - data_counties.groupby(
-                ['state'])['cases'].shift(1)
-        data_counties['case_growth_per_100K'] = 100000 * data_counties[
-            'case_growth'] / data_counties['population']
-        # handle data on deaths
-        data_counties['new_deaths'] = data_counties.groupby(
-            ['state'])['deaths'].shift(0) - data_counties.groupby(
-                ['state'])['deaths'].shift(1)
-        data_counties['new_deaths_per_100K'] = 100000 * data_counties[
-            'new_deaths'] / data_counties['population']
+        if cases:
+            data_counties = pd.read_sql(
+                f'select c.date, c.state,c.cases, p.population from counties c left join county_population p on c.fips = p.fips where c.state in ({binding_string});',
+                con=self.dbc,
+                params=county_list)
+            data_counties['date'] = pd.to_datetime(data_counties['date'])
+            data_counties.sort_values('date', inplace=True)
+            data_counties['case_growth'] = data_counties.groupby(
+                ['state'])['cases'].shift(0) - data_counties.groupby(
+                    ['state'])['cases'].shift(1)
+            data_counties['case_growth_per_100K'] = 100000 * data_counties[
+                'case_growth'] / data_counties['population']
+        else:
+            # handle data on deaths
+            data_counties = pd.read_sql(
+                f'select c.date, c.state,c.deaths, p.population from counties c left join county_population p on c.fips = p.fips where c.state in ({binding_string});',
+                con=self.dbc,
+                params=county_list)
+            data_counties['date'] = pd.to_datetime(data_counties['date'])
+            data_counties.sort_values('date', inplace=True)
+            data_counties['new_deaths'] = data_counties.groupby(
+                ['state'])['deaths'].shift(0) - data_counties.groupby(
+                    ['state'])['deaths'].shift(1)
+            data_counties['new_deaths_per_100K'] = 100000 * data_counties[
+                'new_deaths'] / data_counties['population']
         data = pd.concat([data_states, data_counties])
         data['date'] = pd.to_datetime(data['date'])
         return data.rename(
@@ -175,34 +187,43 @@ def update_line_chart(states, counties, rolling_days, data_type):
     if data_type == 'Cases':
         y_axis_label = "New Reported Cases Per 100,000"
         y_variable = 'rolling_case_growth_per_100K'
+        hover_data = [
+            'date', 'rolling_new_cases', 'rolling_case_growth_per_100K',
+            'New Cases', 'case_growth_per_100K'
+        ]
+        cases = True
     else:
         y_axis_label = "New Reported Deaths Per 100,000"
         y_variable = 'rolling_new_deaths_per_100K'
+        hover_data = [
+            'date', 'rolling_new_deaths_per_100K', 'new_deaths_per_100K'
+        ]
+        cases = False
+
     if len(states_and_counties) > 15:
         #quietly limiting the length of the list to 15
         states_and_counties = states_and_counties[:15]
-    dff = myDataLoader.get_data(states_and_counties).sort_values(
-        ["date", "state"]).reset_index()
+    dff = myDataLoader.get_data(states_and_counties,
+                                cases=cases).sort_values(["date", "state"
+                                                          ]).reset_index()
     # update : still dumb, transform is better than what I had before but why there isn't a clean handoff
     # between rolling groupby and transform I don't know
-    dff["rolling_case_growth_per_100K"] = dff.groupby(
-        'state')['case_growth_per_100K'].transform(
+    if cases:
+        dff["rolling_case_growth_per_100K"] = dff.groupby(
+            'state')['case_growth_per_100K'].transform(
+                lambda s: s.rolling(rolling_days, min_periods=1).mean())
+        dff["rolling_new_cases"] = dff.groupby('state')['New Cases'].transform(
             lambda s: s.rolling(rolling_days, min_periods=1).mean())
-    dff["rolling_new_cases"] = dff.groupby('state')['New Cases'].transform(
-        lambda s: s.rolling(rolling_days, min_periods=1).mean())
-    dff["rolling_new_deaths_per_100K"] = dff.groupby(
-        'state')['new_deaths_per_100K'].transform(
-            lambda s: s.rolling(rolling_days, min_periods=1).mean())
+    else:
+
+        dff["rolling_new_deaths_per_100K"] = dff.groupby(
+            'state')['new_deaths_per_100K'].transform(
+                lambda s: s.rolling(rolling_days, min_periods=1).mean())
     fig = px.line(dff,
                   x="date",
                   y=y_variable,
                   color="state",
-                  hover_data=[
-                      'date', 'rolling_new_cases',
-                      'rolling_case_growth_per_100K', 'New Cases',
-                      'case_growth_per_100K', 'rolling_new_deaths_per_100K',
-                      'new_deaths_per_100K'
-                  ],
+                  hover_data=hover_data,
                   labels={'state': ''})
 
     fig.update_layout(margin={
