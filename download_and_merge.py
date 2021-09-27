28d05626f8f4677494d646e36a5496f048594b1d
Marcos Huerta <sirmarcos@gmail.com>
2021-09-26 21:29:37 -0400

    add deaths data to sql database

--- download_and_merge.py
+++ download_and_merge.py
@@ -75,40 +75,28 @@ def get_state_nyt(pop_data):
     """Considerably smaller data file so merging and pre-computing some data before uploading to sqlite db is practical"""
     state_nyt = pd.read_csv(
         'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
-    ).drop(columns=['deaths', 'fips'])
+    ).drop(columns=['fips'])
     state_nyt['date'] = pd.to_datetime(state_nyt['date'])
     state_nyt.sort_values('date', inplace=True)
     state_nyt['case_growth'] = state_nyt.groupby([
         'state'
     ])['cases'].shift(0) - state_nyt.groupby(['state'])['cases'].shift(1)
-    state_nyt = state_nyt[['date', 'case_growth', 'state'
+    state_nyt['new_deaths'] = state_nyt.groupby([
+        'state'
+    ])['deaths'].shift(0) - state_nyt.groupby(['state'])['deaths'].shift(1)
+    state_nyt = state_nyt[['date', 'case_growth', 'state', 'new_deaths'
                            ]].merge(pop_data[['POPESTIMATE2019', 'NAME']],
                                     left_on='state',
                                     right_on='NAME',
                                     how='left')
-    #   state_nyt['cases_per_100K'] = 100000 * state_nyt['cases'] / state_nyt[
-    #       'POPESTIMATE2019']
+
     state_nyt['case_growth_per_100K'] = 100000 * state_nyt[
         'case_growth'] / state_nyt['POPESTIMATE2019']
+    state_nyt['new_deaths_per_100K'] = 100000 * state_nyt[
+        'new_deaths'] / state_nyt['POPESTIMATE2019']
 
     return state_nyt.sort_values('date').drop(
         columns=['NAME', 'name', 'POPESTIMATE2019'], errors='ignore')
-
-
-def write_county_sql():
-    pop_data_county = pd.read_csv('2019_county_populations.csv')
-    dytpe_dict = {
-        'date': types.TEXT,
-        'fips': types.TEXT,
-        'cases': types.INTEGER,
-        'state': types.TEXT,
-    }
-    pop_data_county.to_sql('county_population',
-                           dbc,
-                           if_exists='replace',
-                           dtype=dytpe_dict)
-    with dbc.connect() as con:
-        _ = con.execute('create index idx_fips on county_population (fips);')
 
 
 def upload_state_to_sql():
