b68eb2b40cfa11b6209f02c20681709f394176f1
Marcos Huerta <marcos@marcoshuerta.com>
2022-08-05 22:47:09 -0400

    use blueprint

--- rest_api.py
+++ rest_api.py
@@ -1,32 +1,42 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
 from dataLoader import dataLoader
 from api_config import api_key
 
 app = Flask(__name__)
-app.config["APPLICATION_ROOT"] = "/covid_api"
+
+bp = Blueprint('api', __name__, template_folder='templates')
+
+#app.config["APPLICATION_ROOT"] =
 myDataLoader = dataLoader()
 
-@app.route('/', methods=['GET'])
+
+@bp.route('/', methods=['GET'])
 def something():
     return "Hello World!"
 
-@app.route(f'/api/v1/{api_key}/all_states', methods=['GET'])
+
+@bp.route(f'/api/v1/{api_key}/all_states', methods=['GET'])
 def api_list_states():
     return (jsonify(myDataLoader.all_states()))
 
 
-@app.route(f'/api/v1/{api_key}/all_counties', methods=['GET'])
+@bp.route(f'/api/v1/{api_key}/all_counties', methods=['GET'])
 def api_list_counties():
     return (jsonify(myDataLoader.all_counties()))
 
 
-@app.route(f'/api/v1/{api_key}/cases/<states>', methods=['GET'])
+@bp.route(f'/api/v1/{api_key}/cases/<states>', methods=['GET'])
 def api_get_states(states):
     state_list = states.split('+')
     return (jsonify(
         myDataLoader.get_data(state_list,
                               cases=True).to_dict(orient='records')))
 
+
+app.register_blueprint(
+    bp,
+    url_prefix="/covid_api",
+)
 
 if __name__ == "__main__":
     app.debug = True
